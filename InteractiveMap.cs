9341d84f2e585f6437184ff5afcafa3120f58f5f
nellya55 <90301237+nellya55@users.noreply.github.com>
2021-09-10 10:32:13 +0600

    Initial commit

---
+++ InteractiveMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveMap : MonoBehaviour
+{
+    [SerializeField] private List<RectTransform> mapPosition;
+    [SerializeField] private RectTransform MapRussia;
+   
+   
+    public void CityCall()
+    {
+        InvokeRepeating("MosowClicK", 0, Time.deltaTime* 1f);
+    }
+    private void MosowClicK()
+    {
+        MapRussia.localPosition = new Vector3(Mathf.Lerp(MapRussia.localPosition.x, mapPosition[0].localPosition.x, Time.deltaTime*1f), Mathf.Lerp(MapRussia.localPosition.y, mapPosition[0].localPosition.y, Time.deltaTime*1f), Mathf.Lerp(MapRussia.localPosition.z, mapPosition[0].localPosition.z, Time.deltaTime*1f));
+        MapRussia.localScale = new Vector3(Mathf.Lerp(MapRussia.localScale.x, mapPosition[0].localScale.x, Time.deltaTime*1f), Mathf.Lerp(MapRussia.localScale.y, mapPosition[0].localScale.y, Time.deltaTime*1f), Mathf.Lerp(MapRussia.localScale.z, mapPosition[0].localScale.z, Time.deltaTime*1f));
+        if(mapPosition[0].localPosition.x - MapRussia.localPosition.x < 10f)
+        {
+            CancelInvoke();
+            print("стоп");
+        }
+    }
+
+ 
+}
---
+++ NewGitest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGitest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("טהוע גסו םא ץונ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
